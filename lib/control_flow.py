d69d1866a23e38111c8459b3578cbbda916aa746
kenedy4 <kennedymusyoki8@gmail.com>
2024-08-30 14:20:31 +0300

    Operational conditional flow errors fixed

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -19,8 +19,26 @@ def hows_the_weather(temperature):
     return "It's too dang hot out there!"
 def fizzbuzz(num):
     # your code here
-    pass
-
+    if num == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
 def calculator(operation, num1, num2):
     # your code here
-    pass
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        return num1 / num2
+    else:
+        print("Invalid operation!")
+        return None
